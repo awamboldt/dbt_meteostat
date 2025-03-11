9408a90b9e1ec1a94f9c241eded3b8572f70f12d
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-03-11 16:34:00 +0100

    changed airport prep to match class order for ease

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -1,14 +1,14 @@
 WITH airports_reorder AS (
     SELECT faa
-    	   , name
-    	   , lat
-    	   , lon
-    	   , alt
-    	   , tz
-    	   , dst
-    	   , city
-    	   , country
-    	   , region
+        ,name
+        ,city
+        ,country
+        ,region
+        ,lat
+        ,lon
+        ,alt
+        ,tz
+        ,dst
     FROM {{ref('staging_airports')}}
 )
 SELECT * FROM airports_reorder
