56e500160457f507f3d829bd6ceb595419e69f9e
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-03-12 13:00:36 +0100

    added mart_weather_weekly

---
+++ models/mart/mart_weather_weekly.sql
@@ -0,0 +1,18 @@
+SELECT
+	airport_code
+	, station_id
+	,season
+	, AVG(avg_temp_c) AS weekly_avg_temp
+	, MIN(min_temp_c) AS weekly_min_temp
+	, MAX(max_temp_c) AS weekly_max_temp
+	, SUM(precipitation_mm) AS weekly_sum_precipitation_mm
+	, SUM(max_snow_mm) AS weekly_sum_max_snow_mm
+	, AVG(avg_wind_direction) AS weekly_avg_wind_direction
+	, AVG(avg_wind_speed_kmh) AS weekly_avg_wind_speed_kmh
+	, MAX(wind_peakgust_kmh) AS weekly_max_wind_peakgust_kmh
+	, AVG(avg_pressure_hpa) AS weekly_avg_pressure_hpa
+	, SUM(sun_minutes) AS weekly_total_sun_minutes
+	, cw AS current_week
+-- consider whether the metric should be Average, Maximum, Minimum, Sum or [Mode](https://wiki.postgresql.org/wiki/Aggregate_Mode)
+FROM {{ref('prep_weather_daily')}}
+GROUP BY (cw, airport_code, station_id, season)
